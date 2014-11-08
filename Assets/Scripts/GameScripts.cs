87ba90f4a24a2d31f154d4a66e2b88be2e644435
Amanda Lange <amandajlange@gmail.com>
2014-11-08 16:03:02 -0500

    Timer

---
+++ Assets/Scripts/GameScripts.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			VictoryGame();
+			return;
+		}
+
+		else
+		{
+			Destroy (gameObject);
+		}
+	}
+
+		void VictoryGame(){
+		Application.LoadLevel ("WinScreen");
+		}
+}
+
+
---
+++ Assets/Scripts/Timer.js
@@ -0,0 +1,65 @@
+﻿#pragma strict
+
+var clockIsPaused : boolean = false;
+var startTime : float; 
+var timeRemaining : float;
+//time is in seconds
+
+function Awake () {
+    startTime = Time.time + 20.0;
+}
+
+function Update () {
+    if(!clockIsPaused)
+    {
+        doCountdown();
+    }
+}
+
+function doCountdown()
+{
+    timeRemaining = startTime - Time.time;
+    if (timeRemaining < 0)
+    {
+        timeRemaining = 0;
+        clockIsPaused = true;
+        TimeIsUp();
+    }
+    ShowTime();
+
+}
+
+function PauseClock()
+{
+    clockIsPaused = true;
+}
+
+function unPauseClock()
+{
+    clockIsPaused = false;
+}
+
+function ShowTime()
+{
+    var minutes : int;
+    var seconds : int;
+    var timeStr : String;
+    minutes = timeRemaining/60;
+    seconds = timeRemaining % 60;
+    timeStr = minutes.ToString() + ":";
+    timeStr += seconds.ToString("D2");
+    guiText.text = timeStr;
+}
+
+function TimeIsUp()
+{
+    print("Time is UP");
+    Application.LoadLevel("gameOver");
+}
+
+function OnGUI () {
+
+    print("Timer start");
+
+}
+
